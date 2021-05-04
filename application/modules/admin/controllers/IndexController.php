a6d1f2479ef1c455d4383526af093f7bbb3b34c8
Tran Duc Nha <ducnha@gmail.com>
2021-05-05 00:12:58 +0700

    Check if identity is not null before get user/pass

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -1,27 +1,35 @@
 <?php
-class Admin_IndexController extends Zend_Controller_Action{
-	
-	public function init(){ 
-        $this->view->BaseUrl=$this->_request->getBaseUrl();
-    } 
+class Admin_IndexController extends Zend_Controller_Action
+{
 
-    public function  preDispatch(){
- 	
+    public function init()
+    {
+        $this->view->BaseUrl = $this->_request->getBaseUrl();
+    }
+
+    public function preDispatch()
+    {
+
         $this->auth = Zend_Auth::getInstance();
         $this->identity = $this->auth->getIdentity();
- 
-        $username= $this->identity->user_username;
-        $password= $this->identity->user_password;
- 
-        $users2 = new Admin_Model_UserAdmin();  
-        if ($users2->num($username, $password)>0) {                     
-        
-        }else{
-              $this->_redirect('/admin/login');exit;
-        }
-     }
 
-    public function indexAction(){ 
+        if ($this->identity) {
+            $username = $this->identity->user_username;
+            $password = $this->identity->user_password;
+
+            $users2 = new Admin_Model_UserAdmin();
+            if ($users2->num($username, $password) > 0) {
+
+            } else {
+                $this->_redirect('/admin/login');exit;
+            }
+        } else {
+            $this->_redirect('/admin/login');exit;
+        }
+    }
+
+    public function indexAction()
+    {
         $intership = new Default_Model_Intership();
         $countintership = $intership->countIntershipByFilter();
         $this->view->numberintership = count($countintership);
@@ -33,8 +41,7 @@ class Admin_IndexController extends Zend_Controller_Action{
         $customer = new Default_Model_Customers();
         $countcustomer = $customer->loadCustomers();
         $this->view->countcustomer = count($countcustomer);
-  
-    } 
-	
+
+    }
 
 }
