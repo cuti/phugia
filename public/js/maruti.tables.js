fb15b052fa366420a9a0a5a7dc0b86528167e1d8
Tran Duc Nha <ducnha@gmail.com>
2021-05-16 23:32:32 +0700

    Not so much

--- public/js/maruti.tables.js
+++ public/js/maruti.tables.js
@@ -1,38 +1,38 @@
 
 $(document).ready(function(){
-	
+
 	$('.data-table').dataTable({
-		"oLanguage": {
-			// "url":"//cdn.datatables.net/plug-ins/1.10.19/i18n/Vietnamese.json"
-			"sProcessing":   "Đang xử lý...",
-			"sLengthMenu":   "Xem _MENU_ mục",
-			"sZeroRecords":  "Không tìm thấy dòng nào phù hợp",
-			"sInfo":         "Đang xem _START_ đến _END_ trong tổng số _TOTAL_ mục",
-			"sInfoEmpty":    "Đang xem 0 đến 0 trong tổng số 0 mục",
-			"sInfoFiltered": "(được lọc từ _MAX_ mục)",
-			"sInfoPostFix":  "",
-			"sSearch":       "Tìm:",
-			"sUrl":          "",
-			"oPaginate": {
-				"sFirst":    "Đầu",
-				"sPrevious": "Trước",
-				"sNext":     "Tiếp",
-				"sLast":     "Cuối"
-			}
+		bJQueryUI: true,
+		bScrollCollapse: true,
+		oLanguage: {
+			oPaginate: {
+				sFirst:    "Đầu",
+				sPrevious: "Trước",
+				sNext:     "Tiếp",
+				sLast:     "Cuối"
+			},
+			sInfo:         "Đang xem _START_ đến _END_ trong tổng số _TOTAL_ mục",
+			sInfoEmpty:    "Đang xem 0 đến 0 trong tổng số 0 mục",
+			sInfoFiltered: "(được lọc từ _MAX_ mục)",
+			sInfoPostFix:  "",
+			sLengthMenu:   "Xem _MENU_ mục",
+			sProcessing:   "Đang xử lý...",
+			sSearch:       "Tìm:",
+			// sUrl:          "https://cdn.datatables.net/plug-ins/1.10.19/i18n/Vietnamese.json",
+			sZeroRecords:  "Không tìm thấy dòng nào phù hợp",
 		},
-		"bJQueryUI": true,
+		"sDom": '<""l>t<"F"fp>',
 		"sPaginationType": "full_numbers",
-		"sDom": '<""l>t<"F"fp>'
-		
+		"sScrollX": "100%",
 	});
-	
+
 	$('input[type=checkbox],input[type=radio],input[type=file]').uniform();
-	
-	//$('select').select2();
-	
+
+	$('select').select2();
+
 	$("span.icon input:checkbox, th input:checkbox").click(function() {
 		var checkedStatus = this.checked;
-		var checkbox = $(this).parents('.widget-box').find('tr td:first-child input:checkbox');		
+		var checkbox = $(this).parents('.widget-box').find('tr td:first-child input:checkbox');
 		checkbox.each(function() {
 			this.checked = checkedStatus;
 			if (checkedStatus == this.checked) {
@@ -42,5 +42,5 @@ $(document).ready(function(){
 				$(this).closest('.checker > span').addClass('checked');
 			}
 		});
-	});	
+	});
 });
