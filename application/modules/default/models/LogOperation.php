ce0686a91ae93f9408decfccac5f4718c90dd63f
Tran Duc Nha <ducnha@gmail.com>
2021-05-27 00:12:10 +0700

    Add log models

---
+++ application/modules/default/models/LogOperation.php
@@ -0,0 +1,40 @@
+<?php
+
+class Default_Model_LogOperation extends Zend_Db_Table_Abstract
+{
+    protected $_name = 'log_operation';
+    protected $_primary = 'log_id';
+
+    /**
+     * Write operation log.
+     *
+     * @param  string $action     User action.
+     * @param  string $username   Current username.
+     * @return mixed              Log id.
+     */
+    public function writeLog($action, $username)
+    {
+        try {
+            $user = new Default_Model_User();
+            $userObj = $user->getUserByUsername($username);
+
+            if ($userObj) {
+                $userId = $userObj['user_id'];
+            } else {
+                throw new Exception('Invalid username.');
+            }
+
+            $data = array(
+                'log_action' => $action,
+                'log_time' => date('Y-m-d H:i:s'),
+                'log_user_id' => $userId,
+            );
+
+            $log_id = $this->insert($data);
+
+            return $log_id;
+        } catch (Exception $err) {
+            throw $err;
+        }
+    }
+}
