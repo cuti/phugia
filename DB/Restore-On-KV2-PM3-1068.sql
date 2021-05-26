0ab5de919985dfbf86e56f55095ec58654a7803d
Tran Duc Nha <nhatd.it2@vnpt.vn>
2021-05-26 16:24:15 +0700

    Add backup/restore scripts on KV2-PM3-1068

---
+++ DB/Restore-On-KV2-PM3-1068.sql
@@ -0,0 +1,4 @@
+﻿USE [master];
+GO
+RESTORE DATABASE [WealthyFamily] FROM DISK = N'E:\GitHub\phu-gia\DB\WealthyFamily.bak' WITH FILE = 1 , MOVE N'WealthyHouse' TO N'C:\Program Files\Microsoft SQL Server\MSSQL11.SQLEXPRESS2012\MSSQL\DATA\WealthyHouse.mdf' , MOVE N'WealthyHouse_log' TO N'C:\Program Files\Microsoft SQL Server\MSSQL11.SQLEXPRESS2012\MSSQL\DATA\WealthyHouse_log.ldf' , RECOVERY , NOUNLOAD , REPLACE , STATS = 10
+GO
